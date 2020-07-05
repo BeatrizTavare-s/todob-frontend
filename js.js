7bf7d74c3c7e60e7965f4a19476b25c243cab4cb
Entrapta-v <beatriz98t@gmail.com>
2020-07-04 23:44:35 -0300

    Primeiro commit do projeto

---
+++ js.js
@@ -0,0 +1,12 @@
+// var button = document.querySelector('btninseretarea');
+// var input = document.querySelector('inputinseretarefa')
+// function exibeMensagem(){
+//     alert('teste')
+// }
+
+document.getElementById('btninseretarea').addEventListener("click", function(){
+    
+});
+// button.addEventListener('click', exibeMensagem())
+
+
